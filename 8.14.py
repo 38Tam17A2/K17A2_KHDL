bf42c0add75d86ece74eeb3505f35ebb4e1ae7ac
Khoa Hoc Du Lieu 17A2 <ttttam.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-11 19:02:28 +0700

    Thêm file

---
+++ 8.14.py
@@ -0,0 +1,7 @@
+N=3
+print("Chuong trinh tinh tong N so nguyen N = ", N)
+x=int(input("Nhap so nguyen thu 1 : "))
+y=int(input("Nhap so nguyen thu 2 : "))
+z=int(input("Nhap so nguyen thu 3 : "))
+S=x+y+z
+print("S = ",S)
